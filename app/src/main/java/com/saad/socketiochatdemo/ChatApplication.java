c1a4bbdad0e4d5e45a00063d4306ddc96b3ab83e
saadkhan0799 <ranasaad0799@gmail.com>
2019-03-07 12:01:07 +0500

    SocketIOChat

---
+++ app/src/main/java/com/saad/socketiochatdemo/ChatApplication.java
@@ -0,0 +1,23 @@
+package com.saad.socketiochatdemo;
+
+import android.app.Application;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
+
+public class ChatApplication extends Application {
+
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket(Constants.CHAT_SERVER_URL);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Socket getSocket() {
+        return mSocket;
+    }
+}
---
+++ app/src/main/java/com/saad/socketiochatdemo/Constants.java
@@ -0,0 +1,5 @@
+package com.saad.socketiochatdemo;
+
+public class Constants {
+    public static final String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
+}
